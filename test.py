17939da8a81b8796be6aa585b0ff54caee4414c7
juanLadino00 <47897922+juanLadino00@users.noreply.github.com>
2019-04-24 15:56:03 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-letrasProposicionales = ["A","a","B","C","c","D","d","E","F","f","G","g"]
-inter = ["-A","-B","C","-D","E","-F","G"]
-tru = []
-for i in inter:
-    if(i[0] in letrasProposicionales):
-        tru.append(i);
-
-print(tru)
